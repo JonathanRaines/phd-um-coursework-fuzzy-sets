b4abbe87769a3e007c0d4b9a9bbd8c83db8ecdfa
Jonathan Raines <jr0278@bristol.ac.uk>
2023-12-07 18:15:33 +0000

    Added doc string to functions.apply()

--- fuzzy_sets/functions.py
+++ fuzzy_sets/functions.py
@@ -7,6 +7,7 @@ from fuzzy_sets.alpha import AlphaRange
 import fuzzy_sets.alpha
 
 
+# TODO: Remove, it's superseded by apply()
 def real_to_real(func: Callable[[float], float], fuzzy_set: FuzzySet) -> FuzzySet:
     """Takes in a function and a fuzzy set and returns a new fuzzy set with the function applied to each member's value
 
@@ -64,7 +65,52 @@ def get_alpha_overlap(alpha_ranges: list[tuple[float, float]]) -> tuple[float, f
 
 
 def apply(func: Callable[[float], float], *input_sets: FuzzySet) -> FuzzySet:
-    """Applies a function to multiple fuzzy sets"""
+    """Applies a function to multiple fuzzy sets using the alpha cut method
+
+    Args:
+        func (Callable[[float], float]): The function to the provided fuzzy sets
+        *input_sets (FuzzySet): The fuzzy sets to apply the function to
+
+    Raises:
+        TypeError: If any fuzzy set does not contain real values
+        ValueError: If the number of fuzzy sets does not match the number of inputs the function expects
+
+    Returns:
+        FuzzySet: The resulting fuzzy set
+
+    Examples:
+        >>> die_high_scores = FuzzySet(
+                                {
+                                    FuzzySetMember(1, 0.1),
+                                    FuzzySetMember(2, 0.2),
+                                    FuzzySetMember(3, 0.4),
+                                    FuzzySetMember(4, 0.6),
+                                    FuzzySetMember(5, 0.8),
+                                    FuzzySetMember(6, 1.0),
+                                }
+                            )
+        >>> print("Die high scores:", die_high_scores)
+        Die high scores: 1/0.1 + 2/0.2 + 3/0.4 + 4/0.6 + 5/0.8 + 6/1.0
+        >>> squared = fuzzy_sets.functions.apply(lambda x: x**2, die_high_scores)
+        >>> print("Squared:", squared)
+        Squared: 1/0.1 + 4/0.2 + 9/0.4 + 16/0.6 + 25/0.8 + 36/1.0
+
+        >>> die_low_scores = FuzzySet(
+                                {
+                                    FuzzySetMember(1, 1.0),
+                                    FuzzySetMember(2, 0.8),
+                                    FuzzySetMember(3, 0.6),
+                                    FuzzySetMember(4, 0.4),
+                                    FuzzySetMember(5, 0.2),
+                                    FuzzySetMember(6, 0.1),
+                                }
+                            )
+        >>> print("Die low scores:", die_low_scores)
+        Die low scores: 1/1.0 + 2/0.8 + 3/0.6 + 4/0.4 + 5/0.2 + 6/0.1
+        >>> added = fuzzy_sets.functions.apply(lambda x, y: x + y, die_high_scores, die_low_scores)
+        >>> print("Added:", added)
+        2/0.1 + 12/0.1 + 3/0.2 + 11/0.2 + 4/0.4 + 10/0.4 + 5/0.6 + 9/0.6 + 6/0.8 + 8/0.8 + 7/1.0
+    """
     # Check all fuzzy sets contain real values
     for fuzzy_set in input_sets:
         if not all(
